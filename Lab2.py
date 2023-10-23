306ccab39ca597a62beb060bde693b27469245a8
Chester <chester27.22@ichat.sp.edu.sg>
2023-10-23 21:25:52 +0800

    Full calculation of temperature code

--- Lab2.py
+++ Lab2.py
@@ -25,18 +25,28 @@ def find_min_max(num_list):
     min_max_list = [min_temp,max_temp]
     return min_max_list
 
-def sort_temperature():
+def sort_temperature(num_list):
     print("sort_temperature")
-
-def calc_median_temperature():
+    num_list.sort()
+    return num_list
+def calc_median_temperature(ascending_list):
     print("calc_median_temperature")
+    if len(ascending_list)%2!=0:
+        median = ascending_list[int(((len(ascending_list)+1)/2)-1)]
+    else:
+        median = (ascending_list[int((len(ascending_list)/2) + 1/2)] + ascending_list[int((len(ascending_list)/2) - 1/2)])/2
 
+    return median
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
+    ascending_list = sort_temperature(num_list)
     print(calc_average(num_list))
     print(find_min_max(num_list))
+    print(calc_median_temperature(ascending_list))
+    print(sort_temperature(num_list))
 
+    
 if __name__ == "__main__":
     main()
