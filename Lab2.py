edbaca1a0c3eea080402d4c4bb8e576a834a9122
Chester <chester27.22@ichat.sp.edu.sg>
2023-10-23 16:38:04 +0800

    Calculation of a list of Temperature by using newly defined functions

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,42 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def calc_average(num_list):
+    print("calc_average")
+    sum = 0
+    length = len(num_list)
+    for num in range (0,length):
+        sum = sum + num_list[num]
+
+    average = sum/length
+    return average
+
+def get_user_input():
+    print("get_user_input")
+    x = input()
+    Listofstr = x.split(",")
+    float_list = list(map(float,Listofstr))
+    return float_list
+
+def find_min_max(num_list):
+    print("find_min_max")
+    min_temp = min(num_list)
+    max_temp = max(num_list)
+    min_max_list = [min_temp,max_temp]
+    return min_max_list
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    print(calc_average(num_list))
+    print(find_min_max(num_list))
+
+if __name__ == "__main__":
+    main()
