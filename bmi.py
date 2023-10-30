fe40df1fc2e2a0602641d792c5408c13740fb237
Chester <chester27.22@ichat.sp.edu.sg>
2023-10-30 20:23:05 +0800

    Bmi with a return value

--- bmi.py
+++ bmi.py
@@ -9,14 +9,17 @@ def calculate_bmi(height, weight):
     if(bmi<18.5):
         print("You are Under Weight")
         w = -1
+        return w
 
     elif(18.5<bmi<=25.0):
         print("You are Normal Weight")
         w = 0
+        return w
 
     elif(25.0<bmi):
         print("You are Over Weight")
         w = 1
+        return w
 
 
 calculate_bmi(height=1.73, weight=57)
