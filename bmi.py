a97fd17563edf065123f80667da019b2227bae7e
Chester <chester27.22@ichat.sp.edu.sg>
2023-10-30 15:03:20 +0800

    Return value for the loop

--- bmi.py
+++ bmi.py
@@ -8,9 +8,15 @@ def calculate_bmi(height, weight):
 
     if(bmi<18.5):
         print("You are Under Weight")
+        w = -1
+
     elif(18.5<bmi<=25.0):
         print("You are Normal Weight")
+        w = 0
+
     elif(bmi<25.0):
         print("You are Over Weight")
+        w = 1
+
 
 calculate_bmi(height=1.73, weight=57)
